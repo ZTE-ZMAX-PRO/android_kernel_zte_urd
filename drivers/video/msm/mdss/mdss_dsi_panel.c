9b8652edf1794390868730252166a9420475f452
franciscofranco <franciscofranco.1990@gmail.com>
2017-06-26 09:12:19 +0200

    display: add a simple api to query the display state (on/off) at any.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -33,6 +33,7 @@
 
 #include "mdss_panel.h"
 #include <linux/proc_fs.h>
+#include <linux/display_state.h>
 
 static struct proc_dir_entry * d_entry;
 static char  module_name[50]={"0"};
@@ -95,6 +96,12 @@ void  mdss_dsi_panel_lcd_proc(struct device_node *node)
 	}
 }
 //-->ZTE_TZB add end, 20150923
+
+bool display_on = true;
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -1307,6 +1314,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1374,6 +1383,8 @@ static int mdss_dsi_post_panel_on(struct mdss_panel_data *pdata)
 		msleep(vsync_period);
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, true);
 	}
+	
+	display_on = false;
 
 end:
 	pr_debug("%s:-\n", __func__);
